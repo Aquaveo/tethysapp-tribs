47f1e3657c8e5cd2d8831c4196465cfe5b15e8c7
Yue Sun <ysun@aquaveo.com>
2026-08-05 00:28:59 -0600

    Swith to JWT auth

--- reactapp/config/tests/mocks/handlers.js
+++ reactapp/config/tests/mocks/handlers.js
@@ -19,24 +19,13 @@ const handlers = [
       ctx.set("Content-Type", "application/json")
     );
   }),
-  rest.get("http://api.test/api/session/", (req, res, ctx) => {
+  rest.get("http://api.test/api/token/", (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({ isAuthenticated: true }),
-      ctx.set("Content-Type", "application/json"),
-      ctx.set(
-        "Set-Cookie",
-        "sessionid=3mp52f19lnnrl1eeyb4b7xlxm9f2id8d; HttpOnly; Path=/; SameSite=Lax"
-      )
-    );
-  }),
-  rest.get("http://api.test/api/csrf/", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.set(
-        "X-CSRFToken",
-        "SxICmOkFldX4o4YVaySdZq9sgn0eRd3Ih6uFtY8BgU5tMyZc7n90oJ4M2My5i7cy"
-      )
+      ctx.json({
+        access: "fake-access",
+        refresh: "fake-refresh"
+      }),
     );
   }),
   rest.get("http://api.test/api/whoami/", (req, res, ctx) => {
