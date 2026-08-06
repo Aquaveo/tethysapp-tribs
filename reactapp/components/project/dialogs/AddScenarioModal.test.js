e505fe451024a605b73e9edf88fbbb3a4bf5919e
Yue Sun <ysun@aquaveo.com>
2026-08-05 21:32:27 -0600

    remove csrf value in tests

--- reactapp/components/project/dialogs/AddScenarioModal.test.js
+++ reactapp/components/project/dialogs/AddScenarioModal.test.js
@@ -1,47 +1,30 @@
 import userEvent from "@testing-library/user-event";
-import { AppContext } from "react-tethys/context";
 import { render, screen, waitFor } from "@testing-library/react";
 
 import AddScenarioModal from "./AddScenarioModal";
 
 it("Creates a modal dialog", () => {
-  render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  render(<AddScenarioModal show />);
   const modal = screen.getByRole("dialog");
   expect(modal).toBeInTheDocument();
 });
 
 it("Creates correct title for the modal dialog", () => {
-  render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  render(<AddScenarioModal show />);
   const modal = screen.getByRole("dialog");
   const title = screen.getByRole("heading", { name: /Add Scenario/ });
   expect(modal).toContainElement(title);
 });
 
 it("Creates submit button with correct title in the modal dialog", () => {
-  render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  render(<AddScenarioModal show />);
   const modal = screen.getByRole("dialog");
   const submitButton = screen.getByRole("button", { name: /Add/ });
   expect(modal).toContainElement(submitButton);
 });
 
 it("Creates correct input fields in the modal dialog", () => {
-  render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  render(<AddScenarioModal show />);
   const modal = screen.getByRole("dialog");
   const nameInput = screen.getByLabelText(/Name/);
   expect(modal).toContainElement(nameInput);
@@ -51,11 +34,7 @@ it("Calls submit and close callbacks when submit button pressed.", async () => {
   const user = userEvent.setup();
   const submitCallback = jest.fn();
   const closeCallback = jest.fn();
-  render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal onSubmit={submitCallback} onClose={closeCallback} show />
-    </AppContext.Provider>
-  );
+  render(<AddScenarioModal onSubmit={submitCallback} onClose={closeCallback} show />);
   const nameInput = screen.getByLabelText(/Name/);
   const submitButton = screen.getByRole("button", { name: /Add/ });
   await user.clear(nameInput);
@@ -74,11 +53,7 @@ it("Resets all fields when Cancel Button is clicked", async () => {
   const user = userEvent.setup();
 
   // render the component the first time to input some test values
-  const { rerender } = render(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  const { rerender } = render(<AddScenarioModal show />);
   const nameInput = screen.getByLabelText(/Name/);
   await user.type(nameInput, "Baz");
 
@@ -86,22 +61,14 @@ it("Resets all fields when Cancel Button is clicked", async () => {
   user.click(cancelButton);
 
   // unmount the component and wait for the Cancel button to disappear
-  rerender(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show={false} />
-    </AppContext.Provider>
-  );
+  rerender(<AddScenarioModal show={false} />);
 
   await waitFor(() => {
     expect(cancelButton).not.toBeInTheDocument();
   });
 
   // rerender the component to show up
-  rerender(
-    <AppContext.Provider value={{ csrf: "12345" }}>
-      <AddScenarioModal show />
-    </AppContext.Provider>
-  );
+  rerender(<AddScenarioModal show />);
 
   await waitFor(() => {
     // Assert that the fields have all been reset
@@ -113,11 +80,7 @@ it("Resets all fields when Cancel Button is clicked", async () => {
 // it("Sets name field based on name of file selected (.stl)", async () => {
 //   const user = userEvent.setup();
 //   const file = new File(["foo"], "foo.stl", { type: "model/stl" });
-//   render(
-//     <AppContext.Provider value={{ csrf: "12345" }}>
-//       <AddScenarioModal show />
-//     </AppContext.Provider>
-//   );
+//   render(<AddScenarioModal show />);
 //   const fileInput = screen.getByLabelText(/File/);
 //   await user.upload(fileInput, file);
 //   const nameInput = screen.getByLabelText(/Name/);
@@ -127,11 +90,7 @@ it("Resets all fields when Cancel Button is clicked", async () => {
 // it("Sets name field based on name of file selected (.gdf)", async () => {
 //   const user = userEvent.setup();
 //   const file = new File(["foo"], "foo.gdf", { type: "text/plain" });
-//   render(
-//     <AppContext.Provider value={{ csrf: "12345" }}>
-//       <AddScenarioModal show />
-//     </AppContext.Provider>
-//   );
+//   render(<AddScenarioModal show />);
 //   const fileInput = screen.getByLabelText(/File/);
 //   await user.upload(fileInput, file);
 //   const nameInput = screen.getByLabelText(/Name/);
