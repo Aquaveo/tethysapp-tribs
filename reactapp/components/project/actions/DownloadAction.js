621dcd42fa1e3502293edc9d0d585f39f5487f0e
Yue Sun <ysun@aquaveo.com>
2026-09-01 00:46:12 -0600

    add Download button to each file in a compound dataset

--- reactapp/components/project/actions/DownloadAction.js
+++ reactapp/components/project/actions/DownloadAction.js
@@ -3,11 +3,14 @@ import { BsDownload } from "react-icons/bs";
 
 import Action from "components/actions/Action";
 
-const DownloadAction = ({ datasetId, title = "Download", ...props }) => {
+const DownloadAction = ({ datasetId, layer, title = "Download", ...props }) => {
   const handleClick = () => {
+    const query = layer
+      ? `tab_action=download_layer&layer=${encodeURIComponent(layer)}`
+      : `tab_action=download_all`;
     window.location.href =
       process.env.TETHYS_APP_ROOT_URL +
-      `datasets/${datasetId}/details/files/?tab_action=download_all`;
+      `datasets/${datasetId}/details/files/?${query}`;
   };
   return <Action title={title} icon={<BsDownload />} onClick={handleClick} {...props} />;
 };
@@ -15,6 +18,7 @@ const DownloadAction = ({ datasetId, title = "Download", ...props }) => {
 DownloadAction.propTypes = {
   datasetId: PropTypes.string.isRequired,
   title: PropTypes.string,
+  layer: PropTypes.string
 };
 
 export default DownloadAction;
