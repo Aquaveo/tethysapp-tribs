f9eea0d7c1ce2e09a75b975d76a68f801a87e02a
Yue Sun <ysun@aquaveo.com>
2026-09-10 02:39:03 -0600

    add Download button and function for Scenarios

--- reactapp/components/project/actions/DownloadAction.js
+++ reactapp/components/project/actions/DownloadAction.js
@@ -3,15 +3,20 @@ import { BsDownload } from "react-icons/bs";
 
 import Action from "components/actions/Action";
 
-const DownloadAction = ({ datasetId, realizationId, layer, title = "Download", ...props }) => {
+const DownloadAction = ({ datasetId, scenarioId, realizationId, layer, title = "Download", ...props }) => {
   const handleClick = () => {
     const query = layer
       ? `tab_action=download_layer&layer=${encodeURIComponent(layer)}`
       : `tab_action=download_all`;
     // Each resource type exposes its download action on a different details tab
-    const path = realizationId
-      ? `realizations/${realizationId}/details/datasets/`
-      : `datasets/${datasetId}/details/files/`;
+    let path;
+    if (realizationId) {
+      path = `realizations/${realizationId}/details/datasets/`;
+    } else if (scenarioId) {
+      path = `scenarios/${scenarioId}/details/datasets/`;
+    } else if (datasetId) {
+      path = `datasets/${datasetId}/details/files/`;
+    }
     window.location.href = process.env.TETHYS_APP_ROOT_URL + `${path}?${query}`;
   };
   return <Action title={title} icon={<BsDownload />} onClick={handleClick} {...props} />;
--- tethysapp/tribs/controllers/scenarios/tabs/tribs_scenario_datasets_tab.py
+++ tethysapp/tribs/controllers/scenarios/tabs/tribs_scenario_datasets_tab.py
@@ -5,11 +5,17 @@
 * Copyright: (c) Aquaveo 2023
 ********************************************************************************
 """
+import os
+import tempfile
+
 from django.urls import reverse
 from tethysext.atcore.controllers.resources import ResourceListTab
 
 
 class TribsScenarioDatasetsTab(ResourceListTab):
+    # Generated visualization artifacts stored alongside the source files in the collections
+    exclude_dirs = ('czml', 'gltf')
+
     def get_resources(self, request, resource, session, *args, **kwargs):
         """
         Get a list of resources
@@ -18,6 +24,26 @@ class TribsScenarioDatasetsTab(ResourceListTab):
             A list of Resources.
         """
         return resource.linked_datasets
+
+    def download_all(self, request, resource, session, *args, **kwargs):
+        """
+        Download all datasets for a given scenario.
+
+        Args:
+            request (HttpRequest): The current request object.
+            resource (Resource): The current Resource.
+            session (Session): The current database session.
+        """
+        with tempfile.TemporaryDirectory() as temp_dir:
+            all_files = []
+            resource.export(temp_dir, with_datasets=True)
+            for root, dirs, files in os.walk(temp_dir):
+                dirs[:] = [d for d in dirs if d not in self.exclude_dirs]
+                for file in files:
+                    abs_path = os.path.join(root, file)
+                    all_files.append((abs_path, os.path.relpath(abs_path, temp_dir)))
+
+            return self._zip_response(all_files, f'{resource.name}.zip')
 
     def get_href_for_resource(self, app_namespace, resource):
         """
