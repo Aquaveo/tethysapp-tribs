347f0a150fb704504b9bacd212602d73e58757ac
Yue Sun <ysun@aquaveo.com>
2026-09-09 23:14:32 -0600

    add download_all controller for Realization

--- tethysapp/tribs/controllers/realizations/tabs/tribs_realization_datasets_tab.py
+++ tethysapp/tribs/controllers/realizations/tabs/tribs_realization_datasets_tab.py
@@ -5,11 +5,16 @@
 * Copyright: (c) Aquaveo 2023
 ********************************************************************************
 """
+import tempfile
+import os
 from django.urls import reverse
 from tethysext.atcore.controllers.resources import ResourceListTab
 
 
 class TribsRealizationDatasetsTab(ResourceListTab):
+    # Generated visualization artifacts stored alongside the source files in the collections
+    exclude_dirs = ('czml', 'gltf')
+
     def get_resources(self, request, resource, session, *args, **kwargs):
         """
         Get a list of resources
@@ -18,6 +23,26 @@ class TribsRealizationDatasetsTab(ResourceListTab):
             A list of Resources.
         """
         return resource.linked_datasets
+
+    def download_all(self, request, resource, session, *args, **kwargs):
+        """
+        Download all datasets for a given realization.
+
+        Args:
+            request (HttpRequest): The current request object.
+            resource (Resource): The current Resource.
+            session (Session): The current database session.
+        """
+        with tempfile.TemporaryDirectory() as temp_dir:
+            all_files = []
+            resource.export(temp_dir, with_datasets=True)
+            for root, dirs, files in os.walk(temp_dir):
+                dirs[:] = [d for d in dirs if d not in self.exclude_dirs]
+                for file in files:
+                    abs_path = os.path.join(root, file)
+                    all_files.append((abs_path, os.path.relpath(abs_path, temp_dir)))
+
+            return self._zip_response(all_files, f'{resource.name}.zip')
 
     def get_href_for_resource(self, app_namespace, resource):
         """
